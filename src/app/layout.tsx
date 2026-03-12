280f6326e3abd13e1f8f68e63b3368b782df9a6e
muyfresh <muyfreshbos@gmail.com>
2026-03-12 12:00:38 +0100

    seo: add global robots meta tags and verify home canonical url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,18 @@ export const metadata: Metadata = {
   },
   itunes: {
     appId: "6759678666"
-  }
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({
