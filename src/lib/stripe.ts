fbcd3deee62c1537f5af17adcac57fb063a62410
muyfresh <muyfreshbos@gmail.com>
2026-03-13 20:16:29 +0100

    fix(web): provide fallback stripe secret key during build time

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+// Utiliza un valor por defecto vacío durante el 'build' de Next.js
+// para evitar el error 'Neither apiKey nor config.authenticator provided'
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "sk_test_placeholder", {
     // @ts-ignore
     apiVersion: "2024-12-18.acacia",
 });
