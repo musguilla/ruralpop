19c49944c62fcae85aa6234def33877796fe8d73
muyfresh <muyfreshbos@gmail.com>
2026-03-15 01:24:36 +0100

    chore(web): remove temporary test scripts breaking build

--- test-mccormick2.ts
+++
@@ -1,7 +0,0 @@
-import { IGNORED_CATALOG_FILES, getTractorFormattedName } from "./src/lib/tractores-data.ts";
-
-const dupX64 = "MC_RP6D_X6.4_Brochure_40p_6725799A1_WEB_ES-2.pdf.inline (1).jpg";
-console.log(getTractorFormattedName(dupX64));
-
-const dupX76 = "MC_RPE7-RPE8_X7.6_Brochure_CleverCab_64p_6749394A1_Web_ES (1).pdf.inline.pdf";
-console.log(getTractorFormattedName(dupX76));
