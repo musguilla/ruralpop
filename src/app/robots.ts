2fc10f2a2f87e1d018e4e80b34b5110f9baf443c
muyfresh <muyfreshbos@gmail.com>
2026-05-12 22:38:20 +0200

    fix(seo): rename sitemap_tractores to sitemap_tractors to avoid index caching issues

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
             'https://www.ruralpop.com/sitemap_index.xml',
             'https://www.ruralpop.com/sitemap_anuncios.xml',
             'https://www.ruralpop.com/sitemap_lonjas.xml',
-            'https://www.ruralpop.com/sitemap_tractores.xml'
+            'https://www.ruralpop.com/sitemap_tractors.xml'
         ],
     };
 }
