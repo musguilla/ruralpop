af990c011ffde070d473ef3d3edf2c97147db3c1
muyfresh <muyfreshbos@gmail.com>
2026-05-15 00:38:02 +0200

    fix(android): restore reanimated buildFromSource patch for 16KB alignment

--- ruralpop-mobile/with16KBPatch.js
+++ ruralpop-mobile/with16KBPatch.js
@@ -1,7 +1,6 @@
-const { withGradleProperties, withProjectBuildGradle } = require('@expo/config-plugins');
+const { withGradleProperties } = require('@expo/config-plugins');
 
 module.exports = function with16KBPatch(config) {
-  // Force Reanimated to build from source so it uses NDK 27 (16KB aligned)
   config = withGradleProperties(config, (config) => {
     config.modResults.push({
       type: 'property',
@@ -10,20 +9,5 @@ module.exports = function with16KBPatch(config) {
     });
     return config;
   });
-
-  // Inject global resolution strategy to fix dependency conflict with newer Stripe versions
-  config = withProjectBuildGradle(config, (config) => {
-    if (config.modResults.language === 'groovy') {
-      config.modResults.contents += `\nallprojects {
-    configurations.all {
-        resolutionStrategy {
-            force 'com.google.android.gms:play-services-wallet:19.4.0'
-        }
-    }
-}\n`;
-    }
-    return config;
-  });
-
   return config;
 };
