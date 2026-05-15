1eef7b867db51001122d36effc6fb66db2d2bba9
muyfresh <muyfreshbos@gmail.com>
2026-05-15 18:13:18 +0200

    fix(android): strictly restore definitive 16KB legacy packaging bypass from knowledge base

--- ruralpop-mobile/with16KBPatch.js
+++ ruralpop-mobile/with16KBPatch.js
@@ -1,13 +1,21 @@
-const { withGradleProperties } = require('@expo/config-plugins');
+const { withGradleProperties, withProjectBuildGradle } = require('@expo/config-plugins');
 
 module.exports = function with16KBPatch(config) {
   config = withGradleProperties(config, (config) => {
-    config.modResults.push({
-      type: 'property',
-      key: 'reanimated.buildFromSource',
-      value: 'true',
-    });
+    config.modResults.push({ type: 'property', key: 'reanimated.buildFromSource', value: 'true' });
     return config;
   });
+
+  config = withProjectBuildGradle(config, (config) => {
+    if (config.modResults.language === 'groovy') {
+      config.modResults.contents += `\nallprojects {
+    configurations.all {
+        resolutionStrategy { force 'com.google.android.gms:play-services-wallet:19.4.0' }
+    }
+}\n`;
+    }
+    return config;
+  });
+
   return config;
 };
