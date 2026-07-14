bd6d18cf2481c29f79da364050489116ec1ddf3b
musguilla <info@musguilla.com>
2026-07-14 21:17:58 +0200

    chore: bump mobile version to 1.0.51 and build number to 100

--- ruralpop-mobile/app.config.ts
+++ ruralpop-mobile/app.config.ts
@@ -1,8 +1,8 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 const IS_EQUIPOP = process.env.APP_VARIANT === 'equipop';
-const VERSION = '1.0.49';
-const BUILD_NUMBER = 98;
+const VERSION = '1.0.51';
+const BUILD_NUMBER = 100;
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   return {
