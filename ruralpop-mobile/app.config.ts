98ee7dfbb0bf024bd8e2762d30c91e3be9413ac0
musguilla <info@musguilla.com>
2026-08-29 00:21:03 +0200

    chore(mobile): bump version to 1.0.62 and build number to 117 due to apple rejection

--- ruralpop-mobile/app.config.ts
+++ ruralpop-mobile/app.config.ts
@@ -1,8 +1,8 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 const IS_EQUIPOP = process.env.APP_VARIANT === 'equipop';
-const VERSION = '1.0.61';
-const BUILD_NUMBER = 116;
+const VERSION = '1.0.62';
+const BUILD_NUMBER = 117;
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   return {
