18fa5ca81c8c460b0967340de93dec86de129597
musguilla <info@musguilla.com>
2026-08-28 12:38:27 +0200

    chore(mobile): bump version to 1.0.61 (build 110)

--- ruralpop-mobile/app.config.ts
+++ ruralpop-mobile/app.config.ts
@@ -1,8 +1,8 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 const IS_EQUIPOP = process.env.APP_VARIANT === 'equipop';
-const VERSION = '1.0.60';
-const BUILD_NUMBER = 109;
+const VERSION = '1.0.61';
+const BUILD_NUMBER = 110;
 
 export default ({ config }: ConfigContext): ExpoConfig => {
   return {
